d50caa8a5f4b37f4b7b549847a840c308017b656
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-23 11:18:20 -0500

    Add remove functionality.

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { unsetBook } from '../redux/books/books';
 
 export default function BooksList() {
   const { bookName, gender } = useSelector((state) => state.books);
   const [stateBooks, addBook] = useState([]);
+  const dispatch = useDispatch();
   const bookProgress = Math.floor(Math.random() * 100);
+  const bookId = uuidv4();
   const newBook = [...stateBooks, {
     gender,
     author: '',
@@ -12,43 +16,45 @@ export default function BooksList() {
     currentChapter: '1',
     chapterName: '',
     progress: bookProgress,
-    token: '',
+    token: bookId,
   }];
   useEffect(() => {
     console.log('useEffect');
     addBook(newBook);
   }, [bookName, gender]);
 
-  console.log(stateBooks);
+  const removeBook = (id) => {
+    dispatch(unsetBook());
+    const oldArray = [...stateBooks];
+    const newArray = oldArray.filter((book) => book.token !== id);
+    addBook(newArray);
+  };
   return (
-    stateBooks.map((data) => {
-      console.log(data);
-      return (
-        <div key={Math.random() * 10000}>
-          <h3 className="book-category">{data.gender}</h3>
-          <h2 className="book-title">{data.bookName}</h2>
-          <h3 className="book-author">Book Author</h3>
-          <div className="book-buttons">
-            <button type="button" className="book-button">Comments</button>
-            <button type="button" className="book-button">Remove</button>
-            <button type="button" className="book-button">Edit</button>
-          </div>
-          <div className="book-progress">
-            <div className="book-progress-bar">Bar</div>
-            <div className="book-progress-bar-fill">Progress</div>
-            <div className="book-progress-percentage">
-              {data.progress}
-              %
-            </div>
-          </div>
-          <div className="book-chapter">
-            <h3 className="book-chapter-title">Current Chapter</h3>
-            <h3 className="book-chapter-number">Chapter 1</h3>
-            <button type="button" className="book-button">UPDATE PROGRESS</button>
-          </div>
-
+    stateBooks.map((data) => (
+      <div key={Math.random() * 10000}>
+        <h3 className="book-category">{data.gender}</h3>
+        <h2 className="book-title">{data.bookName}</h2>
+        <h3 className="book-author">Book Author</h3>
+        <div className="book-buttons">
+          <button type="button" className="book-button">Comments</button>
+          <button type="button" onClick={() => removeBook(data.token)} className="book-button">Remove</button>
+          <button type="button" className="book-button">Edit</button>
         </div>
-      );
-    })
+        <div className="book-progress">
+          <div className="book-progress-bar">Bar</div>
+          <div className="book-progress-bar-fill">Progress</div>
+          <div className="book-progress-percentage">
+            {data.progress}
+            %
+          </div>
+        </div>
+        <div className="book-chapter">
+          <h3 className="book-chapter-title">Current Chapter</h3>
+          <h3 className="book-chapter-number">Chapter 1</h3>
+          <button type="button" className="book-button">UPDATE PROGRESS</button>
+        </div>
+
+      </div>
+    ))
   );
 }
