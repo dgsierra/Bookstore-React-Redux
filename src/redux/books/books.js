04aec3429a20b58ae7172b9aa923b6b3eba03ee0
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-24 15:58:22 -0500

    Fix ESLint

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -8,21 +8,20 @@ export const fetchBooks = createAsyncThunk('bookstore/books/GET', async () => {
   const res = await fetch(URL);
   const data = await res.json();
   console.log(data);
-  // eslint-disable-next-line camelcase
-  const books = Object.keys(data).map((item_id) => ({ item_id, ...data[item_id][0] }));
+  const books = Object.keys(data).map((item) => ({ item, ...data[item][0] }));
   return books;
 });
 
-// const addBook = createAsyncThunk('books/fetchBooks/ADD', async (newBook, thunkAPI) => {
-//   await fetch(URL, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(newBook),
-//   }).then(() => thunkAPI.dispatch(fetchBooks()));
-//   return newBook;
-// });
+const addBook = createAsyncThunk('books/fetchBooks/ADD', async (newBook, thunkAPI) => {
+  await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newBook),
+  }).then(() => thunkAPI.dispatch(fetchBooks()));
+  return newBook;
+});
 
 // const removeBook = createAsyncThunk('books/fetchBooks/REMOVE', async (id, thunkAPI) => {
 //   await fetch(`${URL}/${id}`, {
