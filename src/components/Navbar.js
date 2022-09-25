03ef47df93cd13cd5d5a3819c4304a1476d82f02
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-24 20:21:06 -0500

    Add progress bar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import BooksList from './Books';
 import Categories from './Categories';
 import BookForm from './BooksForm';
@@ -8,14 +9,18 @@ import '../css/Navbar.css';
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar">
-        <h2 className="navbar-logo">Bookstore CMS</h2>
-        <ul className="navbar-elements">
-          <li className="navbar-element"><Link className="navtext" to="/">Books</Link></li>
-          <li className="navbar-element"><Link className="navtext" to="/categories">Categories</Link></li>
-        </ul>
-      </nav>
-
+      <div className="nav-container">
+        <nav className="navbar">
+          <h2 className="navbar-logo">Bookstore CMS</h2>
+          <ul className="navbar-elements">
+            <li className="navbar-element"><Link className="navtext" to="/">Books</Link></li>
+            <li className="navbar-element"><Link className="navtext" to="/categories">Categories</Link></li>
+          </ul>
+        </nav>
+        <div className="user">
+          <FaUser />
+        </div>
+      </div>
       <Routes>
         <Route
           path="/"
